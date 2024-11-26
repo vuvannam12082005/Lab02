0bd20ffb247d8a23d1eb147bd62e59e8d46f33ab
vuvannam12082005 <Nam.VV235610@sis.hust.edu.vn>
2024-11-26 22:45:47 +0700

    Add files via upload

---
+++ Lab03/AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc/DigitalVideoDisc.java
@@ -0,0 +1,71 @@
+package hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class DigitalVideoDisc {
+	
+	private String title;
+	private String category;
+	private String director;
+	private int length;
+	private float cost;
+	private static int nbDigitalVideoDiscs = 0;
+	private int id;
+	
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+		nbDigitalVideoDiscs += 1;
+		this.id = nbDigitalVideoDiscs;
+	}
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+		nbDigitalVideoDiscs += 1;
+		this.id = nbDigitalVideoDiscs; 
+	}
+	public DigitalVideoDisc(String title, String category, String director, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.cost = cost;
+		nbDigitalVideoDiscs += 1;
+		this.id = nbDigitalVideoDiscs;
+	}
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+		nbDigitalVideoDiscs += 1;
+		this.id = nbDigitalVideoDiscs;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+	public int getID() {
+		return id;
+	}
+	public void setTitle(String newTitle) {
+		this.title = newTitle;
+	}
+	public String toString() {
+		return "DVD - " + this.getTitle() + " - " + this.getCategory() + " - " + this.getDirector() + " - " + this.getLength() + ": " + this.getCost() + " $";
+	}
+}
